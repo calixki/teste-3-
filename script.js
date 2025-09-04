05e3cffa0b8c00758dd3d994330619ab5b683e74
calixki <isabela.calixki@escola.pr.gov.br>
2025-09-04 09:05:23 -0300

    ok

---
+++ script.js
@@ -0,0 +1,26 @@
+const avanca = document. querySelectorAll('.btn-proximo');
+const reiniciarBtn = document.getElementbyI ('.btn-reniciar');
+
+    avanca.forEach(button => {
+ button.addEventListener('click,function')(){
+     const aual
+
+
+
+ }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+)
+
